6096b4a5d45038e2edbf0a1accb1aed29496a25c
BioInfoQuant <165479495+bioinfoquant@users.noreply.github.com>
2024-04-29 03:36:04 +0500

    Update padel.sh

--- padel.sh
+++ padel.sh
@@ -1,1 +1,1 @@
-java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/MACCSFingerprinter.xml -dir ./ -file maccs_keys.csv
+java -Xms4G -Xmx16G -Djava.awt.headless=true -Dorg.openscience.cdk.ringsearch.AllRingsFinderTimeout=60000 -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/MACCSFingerprinter.xml -dir ./ -file maccs_keys.csv
